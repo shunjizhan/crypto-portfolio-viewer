37901e6168defc2f9c0a12d31f96f0f1bf4c67da
shunji <shunhahaha@live.com>
2021-04-06 22:56:38 +0800

    added table library and prints the results more nicely

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,26 +1,47 @@
 const { pad } = require('lodash');
+const { table } = require('table');
 
 const sanitizetokenName = c => c.toLowerCase();
 
 const sortByValue = values => Object.entries(values).sort(
-  ([_1, { USD: val1 }], [_2, { USD: val2 }]) => val2 - val1,
+  ([, { USD: val1 }], [, { USD: val2 }]) => val2 - val1,
 );
 
 const filterObj = (obj, func) => Object.fromEntries(
   Object.entries(obj).filter(func),
 );
-const ignoreValueBelow = x => (([key, value]) => value > x);
+const ignoreValueBelow = x => (([, value]) => value > x);
 
 const toFixedDecimal = decimal => x => parseFloat(parseFloat(x).toFixed(decimal));
+const toFixedDecimal1 = toFixedDecimal(1);
 const toFixedDecimal2 = toFixedDecimal(2);
 
+const printValues = values => {
+  const total = values.find(([name]) => name === 'TOTAL')[1].USD;
+
+  const res = values.map(val => {
+    const [name, { count, USD, BTC }] = val;
+    const ratio = toFixedDecimal1((USD * 100) / total);
+
+    return [name, USD, ratio, BTC, count];
+  });
+
+  const headerCell = ['name', 'USD', 'ratio', 'BTC', 'count'];
+  const output = table([headerCell, ...res]);
+
+  console.log(output);
+  // console.table(Object.fromEntries(res));
+};
+
 const printPortfolioNicely = res => {
-  const msgLength = 58;
+  const MSG_LENGTH = 58;
+
   Object.entries(res).forEach(([name, values]) => {
-    console.log('-'.repeat(msgLength));
-    console.log(`₿ ${pad(`${name} tokens `, msgLength - 4)} ₿`);
-    console.log('-'.repeat(msgLength));
-    console.log(values);
+    console.log('-'.repeat(MSG_LENGTH));
+    console.log(`₿ ${pad(`${name} tokens `, MSG_LENGTH - 4)} ₿`);
+    console.log('-'.repeat(MSG_LENGTH));
+
+    printValues(values);
     console.log('');
   });
 };
